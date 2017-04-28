06b19c4df7e7fbe5ad394d96dcc4cc52963d4d4e
degiorgioo <dave_degiorgio@hotmail.com>
2017-04-28 12:41:53 +0200

    Dockerimages finished

---
+++ archlinux/init.py
@@ -0,0 +1,20 @@
+# INIT SCRIPT for Docker
+import os
+import subprocess
+
+TOX_CMD_ARGS = ['tox', '-c', 'tox_integration.ini']
+PIP_CMD_ARGS = ['pip', 'install', '-e', '.']
+
+try:
+
+    # Install Swid-Generator
+    subprocess.call(PIP_CMD_ARGS)
+
+    # Read File-List and start Tox testing session
+    TEST_FILE_LIST = os.environ['TOX_TEST_FILES']
+    TOX_CMD_ARGS.append(TEST_FILE_LIST)
+    subprocess.call(TOX_CMD_ARGS)
+
+except KeyError:
+    print("Please set ENVIRONMENT VAR 'TOX_TEST_FILES'.")
+
